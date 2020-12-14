6c7f542baac9bb3589b77184c60f4a3e5af92b06
GrivaJG <Griva.yg@gmail.com>
2020-12-14 16:47:33 +0300

    git init

---
+++ Assets/Scripts/CardView.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardView : MonoBehaviour
+{
+    public Text title;
+    public Text description;
+    public Text exhibit;
+
+    public Image animalImage;
+
+    public CardModel[] card;
+
+
+
+    public void SelectCard(int numberCard)
+    {
+        title.text = card[numberCard].title;
+        description.text = card[numberCard].description;
+        exhibit.text = card[numberCard].exhibit;
+        animalImage.sprite = card[numberCard].animalImg;
+    }
+}
